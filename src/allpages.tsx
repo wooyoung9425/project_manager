543a69cf6c79bf21ee761b2781f82eb54b241cef
wykim <dndud9425@gmail.com>
2023-01-06 17:11:46 +0900

    [김우영] 필요한 페이지 추가

--- src/allpages.tsx
+++ src/allpages.tsx
@@ -1,15 +1,19 @@
 //선언된 모든 페이지에 대한 모음입니다.
 import Login from "./pages/LoginPage";
 import Main from "./pages/MainPage";
+//관리자
 import ManagerInfoPage from "./pages/ManagerInfoPage";
+//사용자
 import MemberList from "./pages/Member/MemberListPage";
 import MemberView from "./pages/Member/MemberViewPage";
 import MemberConfirm from "./pages/Member/MemberConfirm";
+//고객사
 import CustomerListPage from "./pages/Customer/CustomerListPage";
 import CustomerAdd from "./pages/Customer/CustomerAdd";
 import CustomerProject from "./pages/Customer/CustomerProject";
 import CustomerView from "./pages/Customer/CustomerViewPage";
 import ProjectView from "./pages/Customer/ProjectViewPage";
+//메세지
 import MessageEmailList from "./pages/Message/MessageEmailList";
 import MesseageEmailView from "./pages/Message/MesseageEmailViewPage";
 import MessageEmailNew from "./pages/Message/MessageEmailNew";
@@ -17,6 +21,7 @@ import QnAEmailList from "./pages/Message/QnAEmailList";
 import MesseageQnaView from "./pages/Message/MesseageQnaViewPage";
 import MessageSMSList from "./pages/Message/MessageSMSList";
 import MesseageSMSView from "./pages/Message/MesseageSMSViewPage";
+//통계
 import StatPeriod from "./pages/State/StatPeriod";
 import StatServer from "./pages/State/StatServer";
 
